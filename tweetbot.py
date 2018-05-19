c07b712655f9bc37307438e682d79d6890d4dfb5
Jay Graber <jay@z.cash>
2018-05-19 06:29:24 -0700

    Add function to get tweets by user

--- tweetbot.py
+++ tweetbot.py
@@ -6,6 +6,7 @@ from secrets import *
 #override tweepy.StreamListener to add logic to on_status
 class MyStreamListener(tweepy.StreamListener):
     def on_status(self, status):
+        print(status.id_str)
         print(status.text)
 
 def read_tweets(keyword, api):
@@ -21,12 +22,18 @@ def filter_tweets(keyword, api):
 
 def post_tweet(tweet, api):
     api.update_status(tweet)
+    
+def get_tweets_by_user(user, api):
+    print "Getting tweets by user"
+    tweets = api.user_timeline(screen_name=user)
+    print "Tweets:", tweets
 
 if __name__ == "__main__":
     auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
     auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
     api = tweepy.API(auth)
     
-    post_tweet("Hello world", api)
+    # read_tweets("eprint.iacr.org", api)
+    get_tweets_by_user("fireflixdragoon", api)
     
     
